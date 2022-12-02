7176bec883474f0bc7acd2b05976e82d7a1b4f73
Cod3ist <ZN103@live.mdx.ac.uk>
2022-12-02 11:55:59 +0400

    added widget for location and activities

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,79 @@ class Home extends StatelessWidget {
         toolbarHeight: 70.0,
         backgroundColor: Colors.grey[850],
       ),
+
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(height: 100.0),
+                Row(
+                    children: <Widget>[
+                      SizedBox(width: 20.0),
+                      buildLocation(), ///Placing the Location Widget
+                      SizedBox(width: 40.0),
+                      buildActivities() ///Placing the Activities Widget
+                    ]
+                ),
+              ]
+          ),
+        ),
+      ),
+
     );
   }
+
+
+  Widget buildLocation() {
+    return Container(
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            primary: Colors.black26,
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(150.0),
+            child:Container(
+                padding: EdgeInsets.fromLTRB(5.0, 10.0, 5.0, 10.0),
+                margin: EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    Image.asset('images/location.jpg', height: 100,width: 100),
+                    SizedBox(height: 10.0),
+                    Text('LOCATION')
+                  ],
+                )
+            ),
+          ),
+        )
+    );
+  } ///Widget to access the Location Screen
+
+  Widget buildActivities() {
+    return Container(
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            primary: Colors.black26,
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(150.0),
+            child:Container(
+                padding: EdgeInsets.fromLTRB(5.0, 10.0, 5.0, 10.0),
+                margin: EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    Image.asset('images/activities.jpg', height: 100,width: 100),
+                    SizedBox(height: 10.0),
+                    Text('ACTIVITIES')
+                  ],
+                )
+            ),
+          ),
+        )
+    );
+  }///Widget to access the Activities of the user
+
 }
